3e1a8fac4dd737e72edb4494817b3261fb7a2f48
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2024-01-10 21:08:17 -0500

    moved rabbitmq setup into play.common, updated nuget package to 1.0.1

--- Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,7 +1,6 @@
-using MassTransit;
 using Play.Catalog.Service.Models;
+using Play.Common.MassTransit;
 using Play.Common.MongoDB;
-using Play.Catalog.Service.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,19 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 //     builder.Configuration.GetSection(nameof(CatalogDatabaseSettings)));
 
 // Inject MongoDB database as a singleton.
-builder.Services.AddMongo().AddMongoRepository<Item>("items");
+builder.Services.AddMongo().AddMongoRepository<Item>("items").AddMassTransitWithRabbitMQ();
 
 // Configure MassTransit
-builder.Services.AddMassTransit(x =>
-{
-    x.UsingRabbitMq((context, cfg) =>
-    {
-        var rabbitMQSettings = builder.Configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-        cfg.Host(rabbitMQSettings.Host);
-
-        cfg.ConfigureEndpoints(context);
-    });
-});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Play.Common/src/Play.Common/MassTransit/Extensions.cs
@@ -0,0 +1,28 @@
+using Microsoft.Extensions.DependencyInjection;
+using Play.Common.Settings;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Play.Common.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMassTransitWithRabbitMQ(this IServiceCollection services)
+    {
+        services.AddMassTransit(configure =>
+            {
+                configure.AddConsumers(Assembly.GetEntryAssembly());
+
+                configure.UsingRabbitMq((context, cfg) =>
+                {
+                    var configuration = context.GetService<IConfiguration>();
+                    var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                    cfg.Host(rabbitMQSettings.Host);
+
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+        return services;
+    }
+}
