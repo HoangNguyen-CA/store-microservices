a6794d2d15c733c50fa5ed2dfe6c235fe72e0a9c
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2024-01-14 20:05:04 -0500

    finished consumer functionality and settup in inventory service

--- Play.Inventory/src/Play.Inventory.Service/Program.cs
+++ Play.Inventory/src/Play.Inventory.Service/Program.cs
@@ -3,31 +3,18 @@ using Play.Inventory.Service.Models;
 using Play.Inventory.Service.Clients;
 using Polly;
 using Polly.Timeout;
+using Play.Common.MassTransit;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
-Random jitterer = new();
-
 // Inject MongoDB database as a singleton.
-builder.Services.AddMongo().AddMongoRepository<InventoryItem>("inventoryItems");
+builder.Services.AddMongo()
+.AddMongoRepository<InventoryItem>("inventoryItems")
+.AddMongoRepository<CatalogItem>("catalogItems")
+.AddMassTransitWithRabbitMQ();
 
-builder.Services.AddHttpClient<CatalogClient>(client =>
-{
-    client.BaseAddress = new Uri("http://localhost:5161");
-})
-.AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)), onRetry: (outcome, timespan, retryAttempt) =>
-{
-    Console.WriteLine($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
-}))
-.AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(3, TimeSpan.FromSeconds(15), onBreak: (outcome, timespan) =>
-{
-    Console.WriteLine("Opening the circuit for 15 seconds...");
-}, onReset: () => Console.WriteLine("Closing the circuit...")))
-
-.AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+AddCatalogClient(builder);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -50,3 +37,24 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void AddCatalogClient(WebApplicationBuilder builder)
+{
+    Random jitterer = new();
+
+
+    builder.Services.AddHttpClient<CatalogClient>(client =>
+    {
+        client.BaseAddress = new Uri("http://localhost:5161");
+    })
+    .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000)), onRetry: (outcome, timespan, retryAttempt) =>
+    {
+        Console.WriteLine($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
+    }))
+    .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(3, TimeSpan.FromSeconds(15), onBreak: (outcome, timespan) =>
+    {
+        Console.WriteLine("Opening the circuit for 15 seconds...");
+    }, onReset: () => Console.WriteLine("Closing the circuit...")))
+
+    .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
+}
